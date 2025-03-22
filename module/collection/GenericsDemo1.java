635d1d71b1e2dbc934157dff22f9d3e6a153ef40
Jaimin Merja <jaiminmerja@gmail.com>
2025-03-22 18:05:55 +0530

    new

---
+++ module/collection/GenericsDemo1.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+public class GenericsDemo1 
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(new Integer(100));
+        System.out.println(a);
+    }
+}
